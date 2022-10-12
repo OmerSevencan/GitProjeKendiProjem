13a7ff563c832bcfb6ae52836f7e8ae8b4c4d311
Omer <omer7can@gmail.com>
2022-10-12 15:02:11 +0600

    for dongusu eklendi sana gelen taskin adi yaziliyor

--- src/Gun01/Merhaba.java
+++ src/Gun01/Merhaba.java
@@ -4,6 +4,12 @@ public class Merhaba {
     public static void main(String[] args) {
 
         System.out.println("Merhaba Dunya");
+        System.out.println("Merhaba");
+
+        for (int i = 0 ; i < 5 ;i++){
+            System.out.println("Merhaba Dunya");
+
+        }
 
     }
 }
